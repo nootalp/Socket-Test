7414f9701b8c491aaf1baff36a5deaf66429549a
nootalp <gomestatschgabriel@gmail.com>
2023-09-24 22:25:26 -0300

    Polite

--- src/utils/configServer.js
+++ src/utils/configServer.js
@@ -2,7 +2,7 @@ const projectDirectory = process.cwd();
 
 module.exports = {
   __dirname: projectDirectory,
-  httpPort: 3000,
+  httpPort: 3000 || process.env.PORT,
 
   get URI() {
     return `http://localhost:${this.httpPort}/`;
